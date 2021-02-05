ec43838b5c553c9b3ced2f712853cabe52e2aa1b
William Stenvi <57349904+rush852@users.noreply.github.com>
2021-02-05 14:41:20 +0100

    Add files via upload

---
+++ tables.sql
@@ -0,0 +1,71 @@
+CREATE TABLE Students(
+	idnr VARCHAR(10) NOT NULL PRIMARY KEY,
+	name TEXT NOT NULL,
+	login TEXT NOT NULL,
+	program TEXT NOT NULL
+);
+CREATE TABLE Branches(
+	name TEXT NOT NULL,
+	program TEXT NOT NULL,
+	PRIMARY KEY (name,program)
+);
+CREATE TABLE Courses(
+	code TEXT NOT NULL PRIMARY KEY,
+	name TEXT NOT NULL,
+	credits FLOAT NOT NULL,
+	department TEXT NOT NULL
+);
+CREATE TABLE LimitedCourses(
+	code TEXT NOT NULL PRIMARY KEY REFERENCES Courses,
+	capacity INTEGER NOT NULL
+);
+CREATE TABLE StudentBranches(
+	student TEXT NOT NULL PRIMARY KEY REFERENCES Students,
+	branch TEXT,
+	program TEXT NOT NULL,
+	FOREIGN KEY (branch,program) REFERENCES Branches (name,program)
+);
+CREATE TABLE Classifications(
+	name TEXT NOT NULL PRIMARY KEY
+);
+CREATE TABLE Classified(
+	course TEXT NOT NULL REFERENCES courses (code),
+	classification TEXT NOT NULL REFERENCES classifications (name),
+	PRIMARY KEY (course,classification)
+);
+CREATE TABLE MandatoryProgram(
+	course TEXT NOT NULL REFERENCES Courses (code),
+	program TEXT NOT NULL,
+	PRIMARY KEY (course,program)
+);
+CREATE TABLE MandatoryBranch(
+	course TEXT NOT NULL REFERENCES Courses (code),
+	branch TEXT NOT NULL,
+	program TEXT NOT NULL,
+	PRIMARY KEY (course,branch,program),
+	FOREIGN KEY (branch,program) REFERENCES Branches (name,program)
+);
+CREATE TABLE RecommendedBranch(
+	course TEXT NOT NULL REFERENCES Courses (code),
+	branch TEXT NOT NULL,
+	program TEXT NOT NULL,
+	PRIMARY KEY (course,branch,program),
+	FOREIGN KEY (branch,program) REFERENCES Branches (name,program)
+);
+CREATE TABLE Registered(
+	student VARCHAR(10) NOT NULL REFERENCES Students (idnr),
+	course TEXT NOT NULL REFERENCES Courses (code),
+	PRIMARY KEY(student,course)
+);
+CREATE TABLE Taken(
+	student VARCHAR(10) NOT NULL REFERENCES Students (idnr),
+	course TEXT NOT NULL REFERENCES Courses (code),
+	grade CHAR check (grade = 'U' OR grade = '3' OR grade = '4' OR grade = '5'),
+	PRIMARY KEY (student,course)
+);
+CREATE TABLE WaitingList(
+	student VARCHAR(10) NOT NULL REFERENCES Students (idnr),
+	course TEXT NOT NULL REFERENCES Limitedcourses (code),
+	position SERIAL,
+	PRIMARY KEY (student,course)
+);
---
+++ views.sql
@@ -0,0 +1,131 @@
+CREATE VIEW BasicInformation AS(
+	SELECT idnr,name,login,students.program,branch 
+	FROM students
+	LEFT JOIN studentbranches
+	ON students.idnr = studentbranches.student
+	ORDER BY idnr ASC
+);
+CREATE VIEW FinishedCourses AS(
+	SELECT student,course,grade,credits FROM taken
+	LEFT JOIN courses
+	ON courses.code = taken.course
+);
+CREATE VIEW PassedCourses AS(
+	SELECT * FROM finishedcourses
+	WHERE grade != 'U'
+);
+CREATE VIEW Registrations AS(
+	SELECT student,course,'registered' AS status FROM registered
+	UNION
+	SELECT student,course,'waiting' AS status FROM waitinglist
+	ORDER BY student ASC
+);
+CREATE VIEW UnreadMandatory AS (
+	SELECT idnr AS student,course FROM students
+	LEFT JOIN mandatoryprogram
+	ON students.program  = mandatoryprogram.program
+	WHERE course IS NOT NULL
+	UNION
+	SELECT student,course FROM studentbranches
+	LEFT JOIN mandatorybranch
+	ON studentbranches.program = mandatorybranch.program 
+	AND studentbranches.branch = mandatorybranch.branch
+	WHERE course IS NOT NULL
+	EXCEPT 
+	SELECT student,course FROM passedcourses
+	);
+CREATE VIEW pathToGraduation AS (
+WITH 
+totalcredits AS (SELECT idnr,COALESCE(sum(passedcourses.credits),0) AS totalCredits FROM students
+LEFT JOIN passedcourses
+ON students.idnr = passedcourses.student
+GROUP BY idnr
+ORDER BY idnr),
+
+mandatoryleft AS (SELECT students.idnr,COALESCE(count(unreadmandatory.student),0) AS mandatoryLeft FROM students
+LEFT JOIN unreadmandatory
+ON students.idnr = unreadmandatory.student
+GROUP BY students.idnr
+ORDER BY students.idnr),
+
+mathcredits AS (SELECT student, COALESCE(sum(passedcourses.credits),0) AS mathCredits FROM passedcourses, classified
+WHERE classified.classification = 'math'  AND passedcourses.course = classified.course
+GROUP BY student
+UNION
+SELECT idnr, 0 AS mathCredits FROM students
+EXCEPT 
+SELECT student,0 AS mathCredits FROM passedcourses
+ORDER BY student),
+
+research AS (SELECT student, COALESCE(sum(passedcourses.credits),0) AS researchCredits FROM passedcourses, classified
+WHERE classified.classification = 'research'  AND passedcourses.course = classified.course
+GROUP BY student
+UNION
+SELECT idnr, 0 AS mathCredits FROM students
+EXCEPT 
+SELECT student,0 AS mathCredits FROM passedcourses
+ORDER BY student),
+
+seminar AS (SELECT student, COALESCE(count(passedcourses.credits),0) AS seminarCourses FROM passedcourses, classified
+WHERE classified.classification = 'seminar'  AND passedcourses.course = classified.course
+GROUP BY student
+UNION
+SELECT idnr, 0 AS mathCredits FROM students
+EXCEPT 
+SELECT student,0 AS mathCredits FROM passedcourses
+ORDER BY student),
+
+recommended AS(SELECT student,course FROM studentbranches
+LEFT JOIN recommendedbranch
+ON recommendedbranch.branch = studentbranches.branch AND
+recommendedbranch.program = studentbranches.program),
+recommendedCredits AS(
+SELECT passedcourses.student,passedcourses.credits FROM passedcourses,recommended
+WHERE passedcourses.student = recommended.student
+AND passedcourses.course = recommended.course
+UNION
+SELECT idnr, 0 AS credits FROM students
+EXCEPT 
+SELECT passedcourses.student, 0 AS credits FROM passedcourses,recommended
+WHERE passedcourses.student = recommended.student
+AND passedcourses.course = recommended.course
+ORDER BY student ASC),
+
+qualifiedTrue AS (SELECT totalcredits.idnr , TRUE AS qualified FROM totalcredits,mathcredits,research,seminar,mandatoryleft,recommendedCredits
+WHERE 
+(totalcredits.idnr = mathcredits.student AND mathcredits >= 20)
+AND
+(totalcredits.idnr = research.student AND researchCredits >=10)
+AND
+(totalcredits.idnr = seminar.student AND seminarcourses >=1)
+AND
+(totalcredits.idnr = mandatoryleft.idnr AND mandatoryleft = 0
+AND
+totalcredits.idnr = recommendedCredits.student AND recommendedCredits.credits >= 10)
+GROUP BY totalcredits.idnr),
+
+qualified AS (
+SELECT idnr,qualified FROM qualifiedtrue
+UNION 
+SELECT idnr, FALSE AS qualified FROM students
+EXCEPT
+SELECT idnr, FALSE AS qualified FROM qualifiedTrue)
+
+SELECT totalcredits.idnr,totalcredits.totalcredits,mandatoryleft.mandatoryleft,
+mathcredits.mathCredits,research.researchCredits,seminar.seminarcourses,qualified.qualified
+FROM totalcredits
+LEFT JOIN mandatoryleft
+ON totalcredits.idnr = mandatoryleft.idnr
+LEFT JOIN mathcredits
+ON totalcredits.idnr = mathcredits.student
+LEFT JOIN research
+ON totalcredits.idnr = research.student
+LEFT JOIN seminar
+ON totalcredits.idnr = seminar.student
+LEFT JOIN qualified
+ON totalcredits.idnr = qualified.idnr
+);
+	
+	
+
+
