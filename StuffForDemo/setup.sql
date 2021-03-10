9153c18c816a91b0b2a63af7ec87d7463e8b3ccf
William Stenvi <57349904+rush852@users.noreply.github.com>
2021-03-10 18:40:25 +0100

    Add files via upload

---
+++ StuffForDemo/setup.sql
@@ -0,0 +1,285 @@
+--Table of all programs
+CREATE TABLE Programs(
+	name TEXT NOT NULL PRIMARY KEY,
+	abbr TEXT NOT NULL
+);
+--Table of all departments
+CREATE TABLE Departments(
+	name TEXT NOT NULL PRIMARY KEY,
+	abbr TEXT NOT NULL UNIQUE
+);
+--Table to know which departments host which courses.  
+CREATE TABLE ProgramDepartments(
+	program TEXT NOT NULL REFERENCES Programs (name),
+	department TEXT NOT NULL REFERENCES Departments (name),
+	PRIMARY KEY (program,department)
+);
+--Table of all students
+CREATE TABLE Students(
+	idnr VARCHAR(10) NOT NULL PRIMARY KEY,
+	name TEXT NOT NULL,
+	login TEXT NOT NULL UNIQUE,
+	program TEXT NOT NULL REFERENCES Programs,
+	UNIQUE (idnr,program) 
+);
+--Table of all branches
+CREATE TABLE Branches(
+	name TEXT NOT NULL,
+	program TEXT NOT NULL REFERENCES Programs,
+	PRIMARY KEY (name,program)
+);
+--Table of all courses
+CREATE TABLE Courses(
+	code CHAR(6) NOT NULL PRIMARY KEY,
+	name TEXT NOT NULL,  
+	credits FLOAT NOT NULL,
+	department TEXT NOT NULL REFERENCES Departments
+);
+--Table of all courses that have prerequisites 
+CREATE TABLE Prerequisites(
+	course CHAR(6) REFERENCES Courses (code),
+	prerequisite CHAR(6) REFERENCES Courses (code),
+	PRIMARY KEY (course,prerequisite)
+);
+--Table for all courses that have a limited amount of capacity
+CREATE TABLE LimitedCourses(
+	code CHAR(6) NOT NULL PRIMARY KEY REFERENCES Courses,
+	capacity INTEGER NOT NULL
+);
+--Table for all students that have chosen a branch.
+--FK (branch,program) to see that branch can be chosen from that program
+--FK (student,program) to see that student is reading that program
+CREATE TABLE StudentBranches(
+	student TEXT NOT NULL PRIMARY KEY REFERENCES Students,
+	branch TEXT NOT NULL,
+	program TEXT NOT NULL,
+	FOREIGN KEY (student, program) REFERENCES Students(idnr,program), 
+	FOREIGN KEY (branch,program) REFERENCES Branches (name,program)
+);
+--Table for every course classification
+CREATE TABLE Classifications(
+	name TEXT NOT NULL PRIMARY KEY
+);
+--Table for all courses that are classified as something
+CREATE TABLE Classified(
+	course TEXT NOT NULL REFERENCES courses (code),
+	classification TEXT NOT NULL REFERENCES classifications (name),
+	PRIMARY KEY (course,classification)
+);
+--Table for all courses that have to be read for each program
+CREATE TABLE MandatoryProgram(
+	course TEXT NOT NULL REFERENCES Courses (code),
+	program TEXT NOT NULL REFERENCES Programs,
+	PRIMARY KEY (course,program)
+);
+--Table for all courses that have to be read for each branch
+CREATE TABLE MandatoryBranch(
+	course TEXT NOT NULL REFERENCES Courses (code),
+	branch TEXT NOT NULL,
+	program TEXT NOT NULL REFERENCES Programs,
+	PRIMARY KEY (course,branch,program),
+	FOREIGN KEY (branch,program) REFERENCES Branches (name,program)
+);
+--Table for all courses that are recommended to be read.
+CREATE TABLE RecommendedBranch(
+	course TEXT NOT NULL REFERENCES Courses (code),
+	branch TEXT NOT NULL,
+	program TEXT NOT NULL REFERENCES Programs,
+	PRIMARY KEY (course,branch,program),
+	FOREIGN KEY (branch,program) REFERENCES Branches (name,program)
+);
+--Table for all students that are registered to a course
+CREATE TABLE Registered(
+	student VARCHAR(10) NOT NULL REFERENCES Students (idnr),
+	course TEXT NOT NULL REFERENCES Courses (code),
+	PRIMARY KEY(student,course)
+);
+--Table for all courses each student has taken.
+CREATE TABLE Taken(
+	student VARCHAR(10) NOT NULL REFERENCES Students (idnr),
+	course TEXT NOT NULL REFERENCES Courses (code),
+	grade CHAR NOT NULL check (grade IN ('U', '3', '4', '5')),
+	PRIMARY KEY (student,course)
+);
+--Table for students waiting to get registered to a course
+CREATE TABLE WaitingList(
+	student VARCHAR(10) NOT NULL REFERENCES Students (idnr),
+	course TEXT NOT NULL REFERENCES Limitedcourses (code),
+	position SERIAL,
+	UNIQUE (position,course),
+	PRIMARY KEY (student,course)
+);
+
+--View that displays some basic information about students
+CREATE VIEW BasicInformation AS(
+	SELECT idnr,name,login,students.program,branch 
+	FROM students
+	LEFT JOIN studentbranches
+	ON students.idnr = studentbranches.student
+	ORDER BY idnr ASC
+);
+--View that displays all courses finished by each student
+CREATE VIEW FinishedCourses AS(
+	SELECT student,course,grade,credits FROM taken
+	LEFT JOIN courses
+	ON courses.code = taken.course
+);
+--View that displays all courses passed by each student
+CREATE VIEW PassedCourses AS(
+	SELECT student,course,credits FROM finishedcourses
+	WHERE grade != 'U'
+);
+--View that displays all student registered or waiting to be registered to a course
+CREATE VIEW Registrations AS(
+	SELECT student,course,'registered' AS status FROM registered
+	UNION
+	SELECT student,course,'waiting' AS status FROM waitinglist
+	ORDER BY student ASC
+);
+--View that displays all unread mandatory courses for each student
+CREATE VIEW UnreadMandatory AS (
+	SELECT idnr AS student,course FROM students
+	LEFT JOIN mandatoryprogram
+	ON students.program  = mandatoryprogram.program
+	WHERE course IS NOT NULL
+	UNION
+	SELECT student,course FROM studentbranches
+	LEFT JOIN mandatorybranch
+	ON studentbranches.program = mandatorybranch.program 
+	AND studentbranches.branch = mandatorybranch.branch
+	WHERE course IS NOT NULL
+	EXCEPT 
+	SELECT student,course FROM passedcourses
+	);
+--View that displays info regarding each students progress towards graduation
+CREATE VIEW pathToGraduation AS (
+WITH 
+totalcredits AS (SELECT idnr,COALESCE(sum(passedcourses.credits),0) AS totalCredits FROM students
+LEFT JOIN passedcourses
+ON students.idnr = passedcourses.student
+GROUP BY idnr
+ORDER BY idnr),
+
+mandatoryleft AS (SELECT students.idnr,COALESCE(count(unreadmandatory.student),0) AS mandatoryLeft FROM students
+LEFT JOIN unreadmandatory
+ON students.idnr = unreadmandatory.student
+GROUP BY students.idnr
+ORDER BY students.idnr),
+
+mathcredits AS (SELECT student, COALESCE(sum(passedcourses.credits),0) AS mathCredits FROM passedcourses, classified
+WHERE classified.classification = 'math'  AND passedcourses.course = classified.course
+GROUP BY student
+UNION
+SELECT idnr, 0 AS mathCredits FROM students
+EXCEPT 
+SELECT student,0 AS mathCredits FROM passedcourses
+ORDER BY student),
+
+research AS (SELECT student, COALESCE(sum(passedcourses.credits),0) AS researchCredits FROM passedcourses, classified
+WHERE classified.classification = 'research'  AND passedcourses.course = classified.course
+GROUP BY student
+UNION
+SELECT idnr, 0 AS researchCredits FROM students
+EXCEPT 
+SELECT student,0 AS researchCredits FROM passedcourses
+ORDER BY student),
+
+seminar AS (SELECT student, COALESCE(count(passedcourses.credits),0) AS seminarCourses FROM passedcourses, classified
+WHERE classified.classification = 'seminar'  AND passedcourses.course = classified.course
+GROUP BY student
+UNION
+SELECT idnr, 0 AS seminarCourses FROM students
+EXCEPT 
+SELECT student,0 AS seminarCourses FROM passedcourses
+ORDER BY student),
+
+recommended AS(SELECT student,course FROM studentbranches
+LEFT JOIN recommendedbranch
+ON recommendedbranch.branch = studentbranches.branch AND
+recommendedbranch.program = studentbranches.program),
+recommendedCredits AS(
+SELECT passedcourses.student,passedcourses.credits FROM passedcourses,recommended
+WHERE passedcourses.student = recommended.student
+AND passedcourses.course = recommended.course
+UNION
+SELECT idnr, 0 AS credits FROM students
+EXCEPT 
+SELECT passedcourses.student, 0 AS credits FROM passedcourses,recommended
+WHERE passedcourses.student = recommended.student
+AND passedcourses.course = recommended.course
+ORDER BY student ASC),
+
+qualifiedTrue AS (SELECT totalcredits.idnr , TRUE AS qualified FROM totalcredits,mathcredits,research,seminar,mandatoryleft,recommendedCredits
+WHERE 
+(totalcredits.idnr = mathcredits.student AND mathcredits >= 20)
+AND
+(totalcredits.idnr = research.student AND researchCredits >=10)
+AND
+(totalcredits.idnr = seminar.student AND seminarcourses >=1)
+AND
+(totalcredits.idnr = mandatoryleft.idnr AND mandatoryleft = 0
+AND
+totalcredits.idnr = recommendedCredits.student AND recommendedCredits.credits >= 10)
+GROUP BY totalcredits.idnr),
+
+qualified AS (
+SELECT idnr,qualified FROM qualifiedtrue
+UNION 
+SELECT idnr, FALSE AS qualified FROM students
+EXCEPT
+SELECT idnr, FALSE AS qualified FROM qualifiedTrue)
+
+SELECT totalcredits.idnr AS student,totalcredits.totalcredits,mandatoryleft.mandatoryleft,
+COALESCE(mathcredits.mathCredits,0) AS mathCredits,COALESCE(research.researchCredits,0) AS researchCredits,COALESCE(seminar.seminarcourses,0) AS seminarcourses,qualified.qualified
+FROM totalcredits
+LEFT JOIN mandatoryleft
+ON totalcredits.idnr = mandatoryleft.idnr
+LEFT JOIN mathcredits
+ON totalcredits.idnr = mathcredits.student
+LEFT JOIN research
+ON totalcredits.idnr = research.student
+LEFT JOIN seminar
+ON totalcredits.idnr = seminar.student
+LEFT JOIN qualified
+ON totalcredits.idnr = qualified.idnr
+);
+
+--A bunch of insert, Used for testing.
+
+INSERT INTO Departments VALUES ('Dep1', 'D1');
+INSERT INTO Departments VALUES ('Dep2', 'D2');
+
+INSERT INTO Programs VALUES ('Prog1', 'P1');
+INSERT INTO Programs VALUES ('Prog2', 'P2');
+
+INSERT INTO Students VALUES ('1111111111', 'N1', 'ls1', 'Prog1');
+INSERT INTO Students VALUES ('2222222222', 'N2', 'ls2', 'Prog1');
+INSERT INTO Students VALUES ('3333333333', 'N3', 'ls3', 'Prog2');
+INSERT INTO Students VALUES ('4444444444', 'N4', 'ls4', 'Prog2');
+
+INSERT INTO Courses VALUES ('CCC111', 'C1', 22.5, 'Dep1');
+INSERT INTO Courses VALUES ('CCC222', 'C2', 20,   'Dep1');
+INSERT INTO Courses VALUES ('CCC333', 'C3', 30,   'Dep1');
+INSERT INTO Courses VALUES ('CCC444', 'C4', 25,   'Dep1');
+INSERT INTO Courses VALUES ('CCC555', 'C5', 25,   'Dep1');
+INSERT INTO Courses VALUES ('CCC666', 'C6', 20,   'Dep1');
+INSERT INTO Courses VALUES ('CCC777', 'C7', 20,   'Dep1');
+INSERT INTO Courses VALUES ('CCC888', 'C8', 20,   'Dep1');
+
+INSERT INTO Prerequisites VALUES ('CCC333', 'CCC111');
+INSERT INTO Prerequisites VALUES ('CCC888', 'CCC777');
+
+INSERT INTO LimitedCourses VALUES ('CCC111',1);
+INSERT INTO LimitedCourses VALUES ('CCC222',2);
+INSERT INTO LimitedCourses VALUES ('CCC555',0);
+INSERT INTO LimitedCourses VALUES ('CCC666',1);
+INSERT INTO LimitedCourses VALUES ('CCC777',1);
+
+INSERT INTO Registered VALUES ('1111111111', 'CCC666');
+INSERT INTO Registered VALUES ('3333333333', 'CCC666');
+INSERT INTO Waitinglist VALUES ('2222222222', 'CCC666');
+
+INSERT INTO Taken VALUES ('2222222222', 'CCC222', 4);
+INSERT INTO Taken VALUES ('2222222222', 'CCC111', 5);
+INSERT INTO Taken VALUES ('3333333333', 'CCC777', 5);
+INSERT INTO Taken VALUES ('3333333333', 'CCC888', 'U');
---
+++ StuffForDemo/triggers.sql
@@ -0,0 +1,100 @@
+CREATE VIEW CourseQueuePositions AS(
+	SELECT student, course, position AS place FROM Waitinglist
+);
+
+CREATE FUNCTION try_register() RETURNS trigger AS $$
+    BEGIN
+	-- Check if student is already registered to course
+	IF EXISTS (SELECT * FROM Registrations 
+			   WHERE student = NEW.student AND course = NEW.course)
+	THEN RAISE EXCEPTION 'Already Registered';
+	END IF;
+	-- Check if student already passed course
+	IF NEW.course IN (SELECT passedcourses.course FROM passedcourses
+			   WHERE student = NEW.student)
+	THEN RAISE EXCEPTION 'student has already passed course';
+	END IF;
+    -- Checks for prerequisites
+    IF EXISTS (SELECT prerequisite FROM Prerequisites
+			   WHERE course = NEW.course
+			   EXCEPT
+			   SELECT course FROM passedcourses
+			   WHERE student = NEW.student)
+	THEN RAISE EXCEPTION 'prerequisites missing';
+	END IF;
+
+    -- Check if the course is full.
+    IF NEW.COURSE IN (SELECT LimitedCourses.code FROM LimitedCourses)
+            AND 
+			(
+                (SELECT COUNT(DISTINCT Registered.student) FROM Registered
+                WHERE Registered.course = NEW.Course)
+                >=
+                (SELECT capacity FROM LimitedCourses
+                WHERE LimitedCourses.code = NEW.course)
+            )
+    THEN
+    -- Course full, insert into waitinglist
+    	INSERT INTO WaitingList VALUES (NEW.student, NEW.course,(SELECT COUNT (*) + 1
+																	FROM Waitinglist
+																	WHERE NEW.course = course));
+    ELSE
+    -- Course not full, insert into registered
+        INSERT INTO Registered VALUES (NEW.student, NEW.course);
+        END IF;
+        RETURN NEW;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER register_to_course INSTEAD OF INSERT ON Registrations 
+    FOR EACH ROW EXECUTE FUNCTION try_register();
+
+
+CREATE  FUNCTION unregister_from_course() RETURNS TRIGGER AS $$
+	DECLARE
+		--First person in waitlist or NULL.
+		first_in_queue TEXT = (SELECT student FROM CourseQueuePositions
+				 WHERE course = OLD.course AND place = 1);
+		currentpos INTEGER = (SELECT place FROM CourseQueuePositions
+				 WHERE course = OLD.course AND student = OLD.student);
+	BEGIN
+		-- Check if student is in waitinglist
+		IF OLD.student IN
+			(SELECT coursequeuepositions.student FROM coursequeuepositions
+			 WHERE  coursequeuepositions.course = OLD.course)
+		THEN 
+		-- Delete student from list, and decrease the position for everyone behind 
+			DELETE FROM Waitinglist WHERE student = OLD.student AND course = OLD.course;
+			UPDATE waitinglist
+			SET position = position-1
+			WHERE course = OLD.course AND position > currentpos;
+			RETURN OLD;
+		END IF;
+		-- Check if student is registered
+		IF OLD.student IN(SELECT Registered.student FROM registered WHERE registered.course = OLD.course)
+		THEN
+			-- Deletes student form registered
+			DELETE FROM Registered WHERE student = OLD.student AND course = OLD.course;
+			-- Checks if there is room for new student to be registered from waitinglist
+			IF OLD.course IN (SELECT LimitedCourses.code FROM LimitedCourses)
+			AND (SELECT COUNT(Registered.student) FROM Registered
+				WHERE Registered.course = OLD.course)
+				<
+				(SELECT capacity FROM LimitedCourses
+				WHERE LimitedCourses.code = OLD.course)
+			AND first_in_queue IS NOT NULL 
+			THEN --There is room for new student and there is someone in queue
+				DELETE FROM WaitingList WHERE student = first_in_queue AND course = OLD.course;
+				--Updates the queue position
+				UPDATE waitinglist
+				SET position = position-1
+				WHERE course = OLD.course AND position > 1;
+				INSERT INTO Registered VALUES (first_in_queue, OLD.course);
+			END IF;
+		END IF;
+		RETURN OLD;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER unregister_from_course INSTEAD OF DELETE ON Registrations 
+    FOR EACH ROW EXECUTE FUNCTION unregister_from_course();
